97e7b6d8379ac4006f8b685bfdbb16cdd21b387f
“yopaZH” <user@kp11.ru>
2022-09-20 11:35:26 +0300

    Completed except fact zero still exists

---
+++ app/src/main/java/com/example/calculatorisip35/LaunchScreen.java
@@ -0,0 +1,28 @@
+package com.example.calculatorisip35;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LaunchScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().
+        setContentView(R.layout.activity_launch_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LaunchScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        } , SPLASH_SCREEN);
+    }
+}
